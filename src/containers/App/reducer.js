91517d73635030141653ff314c21342aac72e6cc
Huy Nguyen (Rei Kafa) <nguyenquanghuy15091995@gmail.com>
2018-09-12 22:01:47 +0700

    [T6EW][ReiKafa] - FrontEnd source code
    - dummy data: marketing

--- src/containers/App/reducer.js
+++ src/containers/App/reducer.js
@@ -2,9 +2,16 @@ import * as data from './appData';
 import { mixDataToLessonList } from '../../commons/Functions';
 
 const contractData = require('../../assets/contracts.json');
+const marketingData = require('../../assets/marketing.json');
 
 const initState = {
-  lessons: mixDataToLessonList([contractData], data.lessons),
+  lessons: mixDataToLessonList(
+    [
+      contractData,
+      marketingData
+    ], 
+    data.lessons
+  ),
 };
 
 const appReducer = (state = initState, action) => {
